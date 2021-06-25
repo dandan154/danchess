b2dff143261fe32fba09b652fe2aa638b23f360b
Daniel <danielmcguinness1997@gmail.com>
2021-06-25 21:38:22 +0200

    added test coverage for board.get_board_size and board.change_player

--- test_pieces.py
+++ test_pieces.py
@@ -33,7 +33,33 @@ class TestBoard(unittest.TestCase):
         self.assertEqual(square1, self.board1._board[3][2])
         self.assertEqual(square2, self.board2._board[7][7])
 
+    def test_get_board_size(self):
 
+        val1 = 12345
+        val2 = 99999
+
+        self.board1._board_size = val1
+        self.board2._board_size = val2
+
+        self.assertEqual(val1, self.board1.get_board_size())
+        self.assertEqual(val2, self.board2.get_board_size())
+
+    def test_change_player(self):
+
+        self.board1._cur_player_is_white = True
+        self.board2._cur_player_is_white = False
+
+        self.board1.change_player()
+        self.board2.change_player()
+
+        self.assertEqual(False, self.board1._cur_player_is_white)
+        self.assertEqual(True, self.board2._cur_player_is_white)
+
+        self.board1.change_player()
+        self.board2.change_player()
+
+        self.assertEqual(True, self.board1._cur_player_is_white)
+        self.assertEqual(False, self.board2._cur_player_is_white)
 
 if __name__ == '__main__':
     unittest.main()
