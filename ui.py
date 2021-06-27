2d7f16e529b84dfdc1618e5706ade6c1b9182b7c
Daniel <danielmcguinness1997@gmail.com>
2021-06-27 14:35:44 +0200

    added piece rendering + onclick coordinate determination

--- ui.py
+++ ui.py
@@ -1,5 +1,6 @@
 import arcade
 import pieces
+
 from string import ascii_uppercase
 
 # Screen constants
@@ -15,11 +16,28 @@ SQUARE_WIDTH = 80
 # Piece constants
 PIECE_HEIGHT = 40
 PIECE_WIDTH = 40
+IMAGE_SCALE = 3.0
+
+SPRITE_LOOKUP_DICT = {
+    "B(B)": "bishop-black-16x16.png",
+    "B(W)": "bishop-white-16x16.png",
+    "K(B)": "king-black-16x16.png",
+    "K(W)": "king-white-16x16.png",
+    "N(B)": "knight-black-16x16.png",
+    "N(W)": "knight-white-16x16.png",
+    "P(B)": "pawn-black-16x16.png",
+    "P(W)": "pawn-white-16x16.png",
+    "Q(B)": "queen-black-16x16.png",
+    "Q(W)": "queen-white-16x16.png",
+    "R(B)": "rook-black-16x16.png",
+    "R(W)": "rook-white-16x16.png",
+    "0": None
+}
 
 
-class ChessGame(arcade.Window):
+class ChessView(arcade.View):
     def __init__(self):
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        super().__init__()
         arcade.set_background_color(arcade.color.ALMOND)
 
         self.board = pieces.Board()
@@ -30,8 +48,25 @@ class ChessGame(arcade.Window):
         self.tile_draw_start_x = int(SCREEN_WIDTH / 2) - (SQUARE_WIDTH * ((self.tile_count_x/2)-1) + (SQUARE_WIDTH/2))
         self.tile_draw_start_y = int(SCREEN_HEIGHT / 2) - (SQUARE_HEIGHT * ((self.tile_count_y/2)-1) + (SQUARE_HEIGHT/2))
 
+        self.chess_piece_sprite_list = None
+
     def setup(self):
-        pass
+
+        #Create initial sprite list for pieces
+        self.chess_piece_sprite_list = arcade.SpriteList()
+        oriented_board = self.board.get_board()
+        for x in range(self.tile_count_x):
+            for y in range(self.tile_count_y):
+                piece_img_path = SPRITE_LOOKUP_DICT[str(oriented_board[x][y])]
+                if piece_img_path is not None:
+                    piece_sprite = arcade.Sprite(
+                        "images/{}".format(piece_img_path),
+                        IMAGE_SCALE
+                    )
+                    piece_sprite.center_x = self.tile_draw_start_x + SQUARE_WIDTH * x
+                    piece_sprite.center_y = self.tile_draw_start_y + SQUARE_HEIGHT * y
+
+                    self.chess_piece_sprite_list.append(piece_sprite)
 
     def on_draw(self):
         arcade.start_render()
@@ -69,11 +104,9 @@ class ChessGame(arcade.Window):
                 self.tile_draw_start_y - SQUARE_HEIGHT,
                 arcade.color.BLACK,
                 18,
-
             )
 
         # Draw numbers along side of board
-        # Draw letters along bottom of board
         for y in range(self.tile_count_y):
             arcade.draw_text(
                 str(y+1),
@@ -81,12 +114,30 @@ class ChessGame(arcade.Window):
                 self.tile_draw_start_y + SQUARE_WIDTH * y - 10,
                 arcade.color.BLACK,
                 18,
-
             )
 
+        self.chess_piece_sprite_list.draw()
 
     def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
-        pass
+        clicked_tile = self._calc_board_coord(x, y)
+
+        # Check if a piece has been selected
+        if clicked_tile[0] < 0 or clicked_tile[0] > (self.board.get_board_size()-1):
+            return
+
+        if clicked_tile[1] < 0 or clicked_tile[1] > (self.board.get_board_size()-1):
+            return
+
+        print(clicked_tile)
+
+    def _calc_board_coord(self, x, y):
+        file = x - self.tile_draw_start_x + (SQUARE_WIDTH/2)
+        file = file // SQUARE_WIDTH
+
+        rank = y - self.tile_draw_start_y + (SQUARE_HEIGHT/2)
+        rank = rank // SQUARE_HEIGHT
+
+        return file, rank
 
     def on_mouse_release(self, x: float, y: float, button: int, modifiers: int):
         pass
@@ -94,13 +145,14 @@ class ChessGame(arcade.Window):
     def on_mouse_motion(self, x: float, y: float, dx: float, dy: float):
         pass
 
-class Piece(arcade.Sprite):
-    pass
-
 
 def main():
-    window = ChessGame()
-    window.setup()
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    chess_view = ChessView()
+
+    window.show_view(chess_view)
+    chess_view.setup()
+
     arcade.run()
 
 
